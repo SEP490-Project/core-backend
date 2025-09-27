1a7d3d57391affd9abee22f7b77a0b1eb8d93554
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-09-27 16:25:27 +0700

    feat(repository): Add GetByID, Count, Exists function to generic repository - refactor GetByConditionn to exclude noTracking parameter as gorm does   not support tracking feature like Entity Framework does

--- internal/application/interfaces/irepository/generic_repository.go
+++ internal/application/interfaces/irepository/generic_repository.go
@@ -8,8 +8,13 @@ import (
 
 type GenericRepository[T any] interface {
 	GetAll(ctx context.Context, filter func(*gorm.DB) *gorm.DB, includes []string, pageSize, pageNumber int) ([]T, int64, error)
-	GetByCondition(ctx context.Context, filter func(*gorm.DB) *gorm.DB, includes []string, noTracking bool) (*T, error)
+	GetByID(ctx context.Context, id any, includes []string) (*T, error)
+	GetByCondition(ctx context.Context, filter func(*gorm.DB) *gorm.DB, includes []string) (*T, error)
+	Count(ctx context.Context, filter func(*gorm.DB) *gorm.DB) (int64, error)
+	Exists(ctx context.Context, filter func(*gorm.DB) *gorm.DB) (bool, error)
 	Add(ctx context.Context, entity *T) error
 	Update(ctx context.Context, entity *T) error
+	UpdateByCondition(ctx context.Context, filter func(*gorm.DB) *gorm.DB, updates map[string]any) error
 	Delete(ctx context.Context, entity *T) error
+	DeleteByID(ctx context.Context, id any) error
 }
