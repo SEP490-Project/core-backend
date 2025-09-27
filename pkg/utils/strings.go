2f23b913ee2f9e4833725cdb25e519b9e67d1cae
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-09-27 16:46:37 +0700

    feat(utils): Add utils file strings.go for converting email or specific strings to username format

---
+++ pkg/utils/strings.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+// ToUsernameString converts an email or string to a valid username format
+// (alphanumeric and underscores only)
+func ToUsernameString(input string) string {
+	var base string
+	if strings.Contains(input, "@") {
+		base = strings.Split(input, "@")[0]
+	} else {
+		base = input
+	}
+
+	re := regexp.MustCompile("[^a-zA-Z0-9]+")
+	username := re.ReplaceAllString(base, "_")
+	username = strings.ToLower(username)
+
+	// Trim leading and trailing underscores and replace multiple underscores with a single one
+	username = strings.Trim(username, "_")
+	reUnderscore := regexp.MustCompile(`_+`)
+	username = reUnderscore.ReplaceAllString(username, "_")
+
+	return username
+}
