984ada491b15e6185e32ca5171067edd41219230
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-10-10 12:08:46 +0700

    feat: Add requests/responses dtos for shipping_address, logged_session, and user dtos

--- internal/application/dto/responses/user_response.go
+++ internal/application/dto/responses/user_response.go
@@ -10,16 +10,20 @@ import (
 
 // UserResponse represents user information in responses
 type UserResponse struct {
-	ID        uuid.UUID     `json:"id" example:"550e8400-e29b-41d4-a716-446655440000"`
-	Username  string        `json:"username" example:"john_doe"`
-	Email     string        `json:"email" example:"john@example.com"`
-	Role      enum.UserRole `json:"role" example:"user"`
-	IsActive  bool          `json:"is_active" example:"true"`
-	CreatedAt string        `json:"created_at" example:"2023-01-01T00:00:00Z"`
-	UpdatedAt string        `json:"updated_at" example:"2023-01-01T00:00:00Z"`
-	LastLogin string        `json:"last_login,omitempty" example:"2023-01-01T00:00:00Z"`
+	ID                 uuid.UUID                  `json:"id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	Username           string                     `json:"username" example:"john_doe"`
+	Email              string                     `json:"email" example:"john@example.com"`
+	Role               enum.UserRole              `json:"role" example:"user"`
+	IsActive           bool                       `json:"is_active" example:"true"`
+	CreatedAt          string                     `json:"created_at" example:"2023-01-01T00:00:00Z"`
+	UpdatedAt          string                     `json:"updated_at" example:"2023-01-01T00:00:00Z"`
+	LastLogin          string                     `json:"last_login,omitempty" example:"2023-01-01T00:00:00Z"`
+	CurrentLoginDevice []*string                  `json:"current_login_device,omitempty" example:"[\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\"]"`
+	NumberOfSessions   int                        `json:"number_of_sessions,omitempty" example:"3"`
+	ShippingAddress    []*ShippingAddressResponse `json:"shipping_address,omitempty"`
 }
 
+// ToUserResponse converts User model to UserResponse
 func (ur *UserResponse) ToUserResponse(model *model.User) (userResponse *UserResponse) {
 	userResponse = &UserResponse{
 		ID:        model.ID,
@@ -30,6 +34,16 @@ func (ur *UserResponse) ToUserResponse(model *model.User) (userResponse *UserRes
 		CreatedAt: utils.FormatLocalTime(model.CreatedAt, ""),
 		UpdatedAt: utils.FormatLocalTime(model.UpdatedAt, ""),
 		LastLogin: utils.FormatLocalTime(model.LastLogin, ""),
+	}
+
+	if len(model.ShippingAddress) > 0 {
+		userResponse.ShippingAddress = ShippingAddressResponse{}.ToResponseList(model.ShippingAddress)
+	}
+
+	if len(model.Sessions) > 0 {
+		loggedSessions := LoggedDeviceListResponse{}.ToResponseList(model.Sessions)
+		userResponse.CurrentLoginDevice = append(userResponse.CurrentLoginDevice, loggedSessions...)
+		userResponse.NumberOfSessions = len(model.Sessions)
 	}
 
 	return
