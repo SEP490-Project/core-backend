da71728484ea426f05b6cb5c5073c23e7d475f79
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-10-16 01:29:07 +0700

    hotfix: Add missing Address field to the CreateBrandRequest and (#37)
    UpdateBrandRequest structs
    - Add Contract counting to Brand filtering API
    - Change BrandResponse to BrandDetailResponse in GetBrandByID API

--- internal/application/interfaces/iservice/brand_service.go
+++ internal/application/interfaces/iservice/brand_service.go
@@ -14,7 +14,7 @@ type BrandService interface {
 	CreateBrand(ctx context.Context, request *requests.CreateBrandRequest) (*responses.BrandResponse, error)
 	CreateBrandWithInActiveUsers(ctx context.Context, uow *irepository.UnitOfWork, request *requests.CreateBrandWithUserRequest) (*responses.BrandResponse, error)
 	UpdateBrand(ctx context.Context, brandID uuid.UUID, request *requests.UpdateBrandRequest) (*responses.BrandResponse, error)
-	GetByID(ctx context.Context, brandID uuid.UUID) (*responses.BrandResponse, error)
+	GetByID(ctx context.Context, brandID uuid.UUID) (*responses.BrandDetailResponse, error)
 	GetByFilter(ctx context.Context, request *requests.ListBrandsRequest) ([]responses.BrandResponse, int64, error)
 	UpdateBrandStatus(ctx context.Context, brandID uuid.UUID, status enum.BrandStatus) (*responses.BrandResponse, error)
 }
