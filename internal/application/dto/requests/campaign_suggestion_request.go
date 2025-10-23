144dfb5dc860ec498723e3e93fc16252caae481e
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-10-23 11:44:50 +0700

    refactor(campaign): Refactor campaign suggestion logic - Fix linting warnings in campaign_service.go - Remove unnecessary campaign_suggestion_request.go - Update campaign_handler.go to use the new API endpoint definition   instead - Update router.go to use the new API endpoint definition instead

--- internal/application/dto/requests/campaign_suggestion_request.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-import "github.com/google/uuid"
-
-// CampaignSuggestionRequest represents a request to generate campaign suggestions from a contract
-type CampaignSuggestionRequest struct {
-	ContractID uuid.UUID `json:"contract_id" binding:"required" validate:"required,uuid"`
-}
