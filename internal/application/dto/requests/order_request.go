9bc33fe91982a5520473aa78c11682ddc1b638cc
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-10-25 00:34:58 +0700

    refactor: format code

--- internal/application/dto/requests/order_request.go
+++ internal/application/dto/requests/order_request.go
@@ -4,10 +4,11 @@ import (
 	"core-backend/internal/domain/enum"
 	"core-backend/internal/domain/model"
 	"encoding/json"
+	"time"
+
 	"github.com/aws/smithy-go/ptr"
 	"github.com/google/uuid"
 	"gorm.io/datatypes"
-	"time"
 )
 
 // ===========================ORDER==============================//
