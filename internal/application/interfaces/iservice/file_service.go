28d00855d0977432c684449b8d0cc9c2009c70d9
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-10-30 00:06:22 +0700

    refactor: optimie video chunk processing (#77)
    - Use pointer when passing object data to reduce memory usage
    - Add more logging for better traceability
    - Passing request context to improve cancellation handling

--- internal/application/interfaces/iservice/file_service.go
+++ internal/application/interfaces/iservice/file_service.go
@@ -1,12 +1,15 @@
 package iservice
 
-import "core-backend/internal/application/dto/responses"
+import (
+	"context"
+	"core-backend/internal/application/dto/responses"
+)
 
 type FileService interface {
 	UploadFile(userID string, filePath string, destination string) (string, error)
 	DeleteFile(userID string, fileName string) error
 
 	//Streaming
-	UploadVideoStream(userID string, fileName string, data []byte, isLastChunk bool, action *string) (*responses.PathResponse, error)
-	DeleteVideoStream(userID string, fileName string) error
+	UploadVideoStream(ctx context.Context, userID string, fileName string, data *[]byte, isLastChunk bool, action *string) (*responses.PathResponse, error)
+	DeleteVideoStream(ctx context.Context, userID string, fileName string) error
 }
