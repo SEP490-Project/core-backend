3e62b352cdab2de4950fdb21db609be17cff2454
Kelvin Khanh <148732710+tgkhanhDev@users.noreply.github.com>
2025-10-29 21:36:09 +0700

    refactor: remove startdate, enddate, status of concept (#74)

--- internal/application/interfaces/iservice/concept_service.go
+++ internal/application/interfaces/iservice/concept_service.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ConceptService interface {
-	GetConceptPagination(limit, offset int, search string) ([]model.Concept, int, error)
+	GetConceptPagination(limit, page int, search string) ([]model.Concept, int, error)
 	CreateConcept(dto requests.ConceptRequest) (*model.Concept, error)
 	UpdateConcept(dto requests.ConceptRequest) (*model.Concept, error)
 	DeleteConcept(conceptID string) error
