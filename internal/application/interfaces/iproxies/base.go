573eb939bdae6fdaf0c2f8403e7c3e2b420b3698
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-11-02 19:15:09 +0700

    feat: Add base proxy implementation and add registry for proxies

---
+++ internal/application/interfaces/iproxies/base.go
@@ -0,0 +1,16 @@
+// Package iproxies contains the interfaces for the proxies in the application.
+package iproxies
+
+import (
+	"context"
+	"net/http"
+)
+
+type BaseProxy interface {
+	Get(ctx context.Context, path string, headers map[string]string) (*http.Response, error)
+	Post(ctx context.Context, path string, headers map[string]string, body any) (*http.Response, error)
+	Put(ctx context.Context, path string, headers map[string]string, body any) (*http.Response, error)
+	Patch(ctx context.Context, path string, headers map[string]string, body any) (*http.Response, error)
+	Delete(ctx context.Context, path string, headers map[string]string) (*http.Response, error)
+	SetBaseURL(baseURL string)
+}
