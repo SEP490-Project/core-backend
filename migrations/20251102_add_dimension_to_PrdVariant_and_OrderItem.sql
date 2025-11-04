f63cb7613fcdf57e0a34f5b331655f3dffc55112
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-11-04 13:45:55 +0700

    fix: Fix malformed migrations file

--- migrations/20251102_add_dimension_to_PrdVariant_and_OrderItem.sql
+++ migrations/20251102_add_dimension_to_PrdVariant_and_OrderItem.sql
@@ -1,8 +1,8 @@
 ALTER TABLE product_variants
-ADD COLUMN weight INTERGER PRECISION,    -- in grams
-ADD COLUMN height INTERGER PRECISION,    -- in centimeters
-ADD COLUMN length INTERGER PRECISION,    -- in centimeters
-ADD COLUMN width INTERGER PRECISION;     -- in centimeters
+    ADD COLUMN weight INTEGER,    -- in grams
+    ADD COLUMN height INTEGER,    -- in centimeters
+    ADD COLUMN length INTEGER,    -- in centimeters
+    ADD COLUMN width INTEGER;  -- in centimeters
 
 COMMENT ON COLUMN product_variants.weight IS 'in grams';
 COMMENT ON COLUMN product_variants.height IS 'in centimeters';
@@ -11,12 +11,14 @@ COMMENT ON COLUMN product_variants.width IS 'in centimeters';
 
 
 ALTER TABLE order_items
-ADD COLUMN weight INTERGER PRECISION,    -- in grams
-ADD COLUMN height INTERGER PRECISION,    -- in centimeters
-ADD COLUMN length INTERGER PRECISION,    -- in centimeters
-ADD COLUMN width INTERGER PRECISION;     -- in centimeters
+    ADD COLUMN weight INTEGER,    -- in grams
+    ADD COLUMN height INTEGER,    -- in centimeters
+    ADD COLUMN length INTEGER,    -- in centimeters
+    ADD COLUMN width INTEGER;  -- in centimeters
 
-COMMENT ON COLUMN product_variants.weight IS 'in grams';
-COMMENT ON COLUMN product_variants.height IS 'in centimeters';
-COMMENT ON COLUMN product_variants.length IS 'in centimeters';
-COMMENT ON COLUMN product_variants.width IS 'in centimeters';
+COMMENT ON COLUMN order_items.weight IS 'in grams';
+COMMENT ON COLUMN order_items.height IS 'in centimeters';
+COMMENT ON COLUMN order_items.length IS 'in centimeters';
+COMMENT ON COLUMN order_items.width IS 'in centimeters';
+
+
