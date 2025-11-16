002b7d6d9ad488a47c822a9fdeb5496f78be7580
Kelvin Khanh <148732710+tgkhanhDev@users.noreply.github.com>
2025-11-16 23:25:28 +0700

    Fix/order payment bugs 20251116 (#155)
    * fix: response, add user note to preorder
    
    * fix swaggo doc
    
    * feat: preorder censorshi[

--- internal/presentation/handler/registry.go
+++ internal/presentation/handler/registry.go
@@ -78,7 +78,7 @@ func NewHandlerRegistry(applicationReg *application.ApplicationRegistry, appConf
 		AffiliateLinkHandler:          NewAffiliateLinkHandler(applicationReg.AffiliateLinkService),
 		RedirectHandler:               NewRedirectHandler(applicationReg.ClickTrackingService, appConfig),
 		AffiliateLinkAnalyticsHandler: NewAffiliateLinkAnalyticsHandler(applicationReg.AffiliateLinkAnalyticsService),
-		PreOrderHandler:               NewPreOrderHandler(applicationReg.PreOrderService, applicationReg.InfrastructureRegistry.UnitOfWork),
+		PreOrderHandler:               NewPreOrderHandler(applicationReg.PreOrderService, applicationReg.InfrastructureRegistry.UnitOfWork, applicationReg.StateTransferService),
 		MarketingAnalyticsHandler:     NewMarketingAnalyticsHandler(applicationReg.MarketingAnalyticsService),
 		FacebookSocialHandler:         NewFacebookSocialHandler(appConfig, applicationReg.FacebookSocialService, applicationReg.InfrastructureRegistry.UnitOfWork),
 		TikTokSocialHandler:           NewTikTokSocialHandler(appConfig, applicationReg.TikTokSocialService, applicationReg.InfrastructureRegistry.UnitOfWork),
