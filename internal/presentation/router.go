e9dc60802a519dd5d4bf81f5bf71e40c56bf09f9
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-11-18 12:40:24 +0700

    feat: Add admin role to approve campaigns

--- internal/presentation/router.go
+++ internal/presentation/router.go
@@ -450,7 +450,7 @@ func (r *Router) setupCampaignRoutes(group *gin.RouterGroup) {
 	}
 
 	brandGroup := campaigns.Group("")
-	brandGroup.Use(r.middlewareRegistry.Auth.RequireRole(brand))
+	brandGroup.Use(r.middlewareRegistry.Auth.RequireRole(brand, admin))
 	{
 		brandGroup.GET("/brand/profile", campaignHandler.GetCampaignsByBrandProfile)
 		brandGroup.PATCH("/id/:id/approve", campaignHandler.ApproveCampaign)
