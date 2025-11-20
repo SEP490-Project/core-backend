2ba413fd0bdb9b304c7dc9985ec51bebdc3d70c5
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-11-20 17:41:04 +0700

    refactor: format code and update Swagger docs

--- migrations/20251109_add_order_note.sql
+++ migrations/20251109_add_order_note.sql
@@ -1,9 +1,9 @@
 ALTER TABLE orders
-    ADD COLUMN action_notes JSONB;
-    ADD COLUMN user_note TEXT;
-
+    ADD COLUMN action_notes JSONB,
+    ADD COLUMN user_note TEXT,
     ADD COLUMN ghn_order_code TEXT;
 
 ALTER TABLE order_items
-    ADD COLUMN item_recap_description TEXT;
+    ADD COLUMN item_recap_description TEXT,
     ADD COLUMN item_recap_image TEXT;
+
