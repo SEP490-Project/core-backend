79e859128c7e20859f999d2688f5f3f061b4f89d
tgkhanhDev <trangiangkhanh175.work@gmail.com>
2025-12-03 07:45:56 +0700

    feat:

--- internal/domain/model/limited_product.go
+++ internal/domain/model/limited_product.go
@@ -11,7 +11,7 @@ type LimitedProduct struct {
 	PremiereDate          time.Time `json:"premiere_date" gorm:"column:premiere_date;not null"`
 	AvailabilityStartDate time.Time `json:"availability_start_date" gorm:"column:availability_start_date;not null"`
 	AvailabilityEndDate   time.Time `json:"availability_end_date" gorm:"column:availability_end_date;not null"`
-
+	AchievableQuantity    int       `json:"achievable_quantity" gorm:"column:achievable_quantity;not null"`
 	// Relationships
 	Product Product `json:"-" gorm:"foreignKey:Id;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 
