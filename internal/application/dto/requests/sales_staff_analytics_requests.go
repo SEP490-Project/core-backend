689f3c81a00cbf4905ae0c25b711ae96e23441d4
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-12-06 02:34:32 +0700

    refactor: update sales staff analytics service and repository
    - refactor sales staff analytics service, repository, and response to
      accomodate new requirements and response structures

--- internal/application/dto/requests/sales_staff_analytics_requests.go
+++ internal/application/dto/requests/sales_staff_analytics_requests.go
@@ -9,8 +9,10 @@ type SalesDashboardFilter struct {
 	FromDateStr *string `form:"from_date" json:"from_date"`
 	ToDateStr   *string `form:"to_date" json:"to_date"`
 	Limit       int     `form:"limit" json:"limit"`               // Default 5
-	PeriodGap   string  `form:"period_gap" json:"period_gap"`     // day, week, month, quarter, year
-	CompareWith string  `form:"compare_with" json:"compare_with"` // previous day/week/month/quarter/year
+	PeriodGap   string  `form:"period_gap" json:"period_gap"`     // day, week, month, quarter, year, all
+	CompareWith string  `form:"compare_with" json:"compare_with"` // previous day/week/month/quarter/year, and if all (no comparison)
+	SortBy      string  `form:"sort_by" json:"sort_by"`           // revenue, units
+	SortOrder   string  `form:"sort_order" json:"sort_order"`     // asc, desc
 
 	// Internal uses
 	FromDate *time.Time `json:"-"`
