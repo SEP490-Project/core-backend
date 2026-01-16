41a6cf527939b5836c0aff3d592d1b357efe8312
tgkhanhDev <trangiangkhanh175.work@gmail.com>
2026-01-16 08:39:00 +0700

    feat: location, limited product announcement

--- internal/application/dto/requests/location_request.go
+++ internal/application/dto/requests/location_request.go
@@ -50,3 +50,22 @@ func (iar *InputAddressRequest) ToModel(userID uuid.UUID, ward model.Ward, distr
 		UserID: userID,
 	}
 }
+
+// UpdateAddressRequest represents the request payload for updating a shipping address
+type UpdateAddressRequest struct {
+	AddressType  enum.AddressType `json:"type" validate:"omitempty,oneof=BILLING SHIPPING" example:"SHIPPING"`
+	FullName     string           `json:"full_name" validate:"omitempty"`
+	PhoneNumber  string           `json:"phone_number" validate:"omitempty"`
+	Email        string           `json:"email"`
+	Street       string           `json:"street" validate:"omitempty"`
+	AddressLine2 string           `json:"address_line_2" example:"Apartment, suite, unit, building, floor, etc."`
+	City         string           `json:"city" validate:"omitempty"`
+	PostalCode   *string          `json:"postal_code"`
+	Country      *string          `json:"country"`
+	IsDefault    *bool            `json:"is_default"`
+
+	//from GHN
+	GhnProvinceID *int    `json:"ghn_province_id" example:"202"`
+	GhnDistrictID *int    `json:"ghn_district_id" example:"3176"`
+	GhnWardCode   *string `json:"ghn_ward_code" example:"21015"`
+}
